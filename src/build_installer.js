f8ffe3e336b573c1f237e057a78c7bcaae541c7d
Mahmoud <mahmoudsharf55@gmail.com>
2021-06-30 10:51:05 +0200

    Build installer done

--- src/build_installer.js
+++ src/build_installer.js
@@ -1,34 +1,23 @@
 const { MSICreator } = require('electron-wix-msi');
-const path = require('path');
 
-// 2. Define input and output directory.
-// Important: the directories must be absolute, not relative e.g
-// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64",
-const APP_DIR = 'dist/win/Lands-Co Collector-win32-x64';
-// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer",
-const OUT_DIR = path.resolve(__dirname, './windows_installer');
+const APP_DIR = 'C:\\Users\\gnozom\\WebstormProjects\\Lands-Co-Collector\\dist\\win\\Lands-Co Collector-win32-x64';
+const OUT_DIR = 'C:\\Users\\gnozom\\WebstormProjects\\Lands-Co-Collector\\dist\\installer';
 
-// 3. Instantiate the MSICreator
 const msiCreator = new MSICreator({
     appDirectory: APP_DIR,
     outputDirectory: OUT_DIR,
-
-    // Configure metadata
-    description: 'This is a demo application',
+    description: 'Lands-Co Collector',
     exe: 'Lands-Co Collector',
-    name: 'Our Code World Desktop App',
-    manufacturer: 'Our Code World Inc',
+    name: 'Lands-Co Collector',
+    defaultInstallMode: 'perUser',
+    manufacturer: 'Ma7MOoOD SHaRaF',
     version: '1.0.0',
-
-    // Configure installer User Interface
+    appIconPath: 'C:\\Users\\gnozom\\WebstormProjects\\Lands-Co-Collector\\src\\assets\\lands-co.ico',
+    programFilesFolderName: "Lands-Co",
     ui: {
-        chooseDirectory: true
+        chooseDirectory: true,
     },
 });
-
-// 4. Create a .wxs template file
-msiCreator.create().then(function(){
-
-    // Step 5: Compile the template to a .msi file
+msiCreator.create().then(function () {
     msiCreator.compile();
 });
